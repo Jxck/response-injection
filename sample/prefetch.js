fca2d522bf6cfbd7afc7bf268a1d7b517e752438
Jxck <block.rxckin.beats@gmail.com>
2015-03-29 19:02:01 +0900

    add update index.html

---
+++ sample/prefetch.js
@@ -0,0 +1,31 @@
+if ('ServiceWorkerGlobalScope' in self && self instanceof ServiceWorkerGlobalScope) {
+  console.log('service worker');
+  self.onfetch = function(event) {
+    var url = new URL(event.request.url);
+    if (!(url.pathname === '/' || url.pathname === '/index.html')) {
+      console.log('no prefetch for', url.pathname);
+      return;
+    }
+    console.log('fetch for', url.pathname);
+    event.respondWith(fetch(event.request.clone()).then(function (response) {
+      var responseInit = {
+        statue: response.status,
+        statusText: response.statusText,
+        headers: response.headers
+      }
+      return response.clone().body.getReader().read().then(function(body) {
+        var bodyInit = String.fromCharCode.apply(null, body.value);
+        console.log(bodyInit);
+        var res = new Response(bodyInit, responseInit);
+        return res;
+      });
+    }));
+  };
+} else {
+  Promise.race([
+      navigator.serviceWorker.register('prefetch.js', { scope: '.' }),
+      navigator.serviceWorker.getRegistration()
+  ]).then(function() {
+    console.log('service worker ready');
+  }).catch(console.error.bind(console));
+}
