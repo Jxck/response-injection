df787406cdb9a87387b5c345cfbe4d42a4d3ab55
Jxck <block.rxckin.beats@gmail.com>
2015-03-29 19:11:03 +0900

    fixup response body

--- sample/prefetch.js
+++ sample/prefetch.js
@@ -16,9 +16,6 @@ if ('ServiceWorkerGlobalScope' in self && self instanceof ServiceWorkerGlobalSco
       return response.clone().body.getReader().read().then(function(body) {
         var bodyString = String.fromCharCode.apply(null, body.value);
         var res = new Response(body.value, responseInit);
-        setTimeout(function() {
-          console.log(bodyString);
-        }, 1000);
         return res;
       });
     }));
